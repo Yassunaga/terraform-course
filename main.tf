cab7e3158fa9529d2e86002eb3d3f0915c23b48e
Gabriel Yassunaga <gabriel_yassunaga@hotmail.com>
2022-05-19 22:11:14 -0300

    Added bucket creation

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_version = "1.2.0"
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.14.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+  profile = "terraform"
+}
+
+resource "aws_s3_bucket" "my-test-bucket" {
+  bucket = "my-tf-test-bucket-3213123"
+
+ tags = {
+   Name = "My bucket"
+   Environment = "Dev"
+ }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.my-test-bucket.id
+  acl = "private"
+}
